58f22169cefe8e0331e5b8b5603ecc0f5b27bc22
Сергей Субботин <zarodiny@yandex.ru>
2025-01-22 09:56:00 +1000

    feat: добавляет сборку

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,37 @@
-import { Inter } from 'next/font/google';
+// import { Inter } from 'next/font/google';
+import localFont from "next/font/local";
+import { Montserrat, Manrope } from 'next/font/google';
 import './globals.css';
 
 import { Footer, Header } from './components';
 
-const inter = Inter({ subsets: ['latin'] })
+// const montserrat = Montserrat({ subsets: ['latin'] })
+
+// const Manrope = localFont({
+//   src: "./fonts/Manrope-Regular.woff2",
+//   variable: '--font-family',
+//   weight: "100 900",
+// });
+
+const manrope = Manrope({
+  weight: ['400', '500', '600', '700', '800'],
+  style: ['normal'],
+  display: 'swap',
+  subsets: ['cyrillic', 'cyrillic-ext'],
+});
+
+const montserrat = Montserrat({
+  weight: ['400', '500', '600', '700'],
+  style: ['normal'],
+  display: 'swap',
+  subsets: ['cyrillic', 'cyrillic-ext'],
+});
+
+const Trajan = localFont({
+  src: "./fonts/trajan_pro_3.woff2",
+  variable: '--second-family',
+  weight: "100 900",
+});
 
 export const metadata = {
   title: 'Дубровина',
@@ -13,7 +41,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ru">
-      <body className={inter.className}>
+      <body className={`${manrope.className} ${montserrat.variable} ${Trajan.variable}`}>
         <Header />
         {children}
         <Footer />
