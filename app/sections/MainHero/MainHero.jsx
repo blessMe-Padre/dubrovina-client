9754a39755773498b28bd6ae98a0f5dbe491d8aa
Сергей Субботин <zarodiny@yandex.ru>
2025-01-23 14:18:44 +1000

    feat: добавляет адаптив

--- app/sections/MainHero/MainHero.jsx
+++ app/sections/MainHero/MainHero.jsx
@@ -5,7 +5,7 @@ import styles from './style.module.css';
 
 const MainHero = () => {
     return (
-        <div>
+        <section>
             <div className={styles.section}>
                 <Image
                     aria-hidden
@@ -17,41 +17,48 @@ const MainHero = () => {
                 />
                 <div className="container relative">
                     <div className={styles.hero_wrapper}>
-                        <h1 className={styles.title}>Экспертный подход и&nbsp;премиальное качество</h1>
-                        <p className={styles.subtitle}>Стоматологическое лечение с душой в Находке</p>
+                        <div>
+                            <h1 className={styles.title}>Экспертный подход и&nbsp;премиальное качество</h1>
+                            <p className={styles.subtitle}>Стоматологическое лечение с душой в Находке</p>
+                        </div>
+                        <button>Хочу красивую улыбку</button>
                     </div>
 
-                    <div className={`${styles.hero_item} ${styles.hero_item_1}`}>
-                        <Image
-                            src="/icons/plus.svg"
-                            alt="плюс"
-                            width={30}
-                            height={30}
-                        />
-                        <span>Превосходные результаты</span>
+                    <div className={styles.button_wrapper}>
+                        <div className={`${styles.hero_item} ${styles.hero_item_1}`}>
+                            <Image
+                                src="/icons/plus.svg"
+                                alt="плюс"
+                                width={30}
+                                height={30}
+                                className={styles.icon}
+                            />
+                            <span>Превосходные результаты</span>
+                        </div>
+                        <div className={`${styles.hero_item} ${styles.hero_item_2}`}>
+                            <Image
+                                src="/icons/plus.svg"
+                                alt="плюс"
+                                width={30}
+                                height={30}
+                                className={styles.icon}
+                            />
+                            <span>Цифровое оснащение клиники</span>
+                        </div>
+                        <div className={`${styles.hero_item} ${styles.hero_item_3}`}>
+                            <Image
+                                src="/icons/plus.svg"
+                                alt="плюс"
+                                width={30}
+                                height={30}
+                                className={styles.icon}
+                            />
+                            <span>Исключительный сервис</span>
+                        </div>
                     </div>
-                    <div className={`${styles.hero_item} ${styles.hero_item_2}`}>
-                        <Image
-                            src="/icons/plus.svg"
-                            alt="плюс"
-                            width={30}
-                            height={30}
-                        />
-                        <span>Цифровое оснащение клиники</span>
-                    </div>
-                    <div className={`${styles.hero_item} ${styles.hero_item_3}`}>
-                        <Image
-                            src="/icons/plus.svg"
-                            alt="плюс"
-                            width={30}
-                            height={30}
-                        />
-                        <span>Исключительный сервис</span>
-                    </div>
-
                 </div>
             </div>
-        </div>
+        </section>
     )
 }
 
