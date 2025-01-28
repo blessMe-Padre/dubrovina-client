a4db368008d1dc23e8919b740b4ee572fbbda4b6
Сергей Субботин <zarodiny@yandex.ru>
2025-01-28 11:29:35 +1000

    feat: добавляет адаптив

--- app/sections/Specialists/Specialists.jsx
+++ app/sections/Specialists/Specialists.jsx
@@ -9,6 +9,7 @@ import { Navigation } from 'swiper/modules'
 import SwiperNavButtons from "../../components/SwiperNavButtons/SwiperNavButtons";
 import 'swiper/css';
 import 'swiper/css/navigation';
+import Link from "next/link";
 
 export default function Specialists() {
 
@@ -23,42 +24,114 @@ export default function Specialists() {
                         spaceBetween={20}
                         slidesPerView={3}
                         modules={[Navigation]}
+                        breakpoints={{
+                            320: { slidesPerView: 1 },
+                            560: { slidesPerView: 2 },
+                            768: { slidesPerView: 3 },
+                        }}
                     >
-                        <SwiperSlide>
-                            <Image
-                                src="/remove_from_server/image-3.jpg"
-                                alt="specialist_1"
-                                width={466}
-                                height={342}
-                                className={`dsv-image`}
-                            />
+                        <SwiperSlide className={styles.item}>
+                            <Link href="/">
+                                <div className={styles.item_image}>
+                                    <Image
+                                        src="/remove_from_server/image-3.jpg"
+                                        alt="specialist_1"
+                                        width={466}
+                                        height={342}
+                                        className={`dsv-image`}
+                                    />
+                                </div>
+                                <div className={styles.item_content}>
+                                    <div className={styles.item_inner}>
+                                        <h3 className={styles.item_title}>дубровина Анастасия александровна</h3>
+                                        <svg width="36" height="35" viewBox="0 0 36 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M8.9007 26.289L26.5784 8.61133" stroke="#27272A" strokeWidth="2" />
+                                            <path d="M10.6826 8.55835H27.7992" stroke="#27272A" strokeWidth="2" />
+                                            <path d="M26.7861 24.6619V7.54529" stroke="#27272A" strokeWidth="2" />
+                                        </svg>
+                                    </div>
+                                    <div className="border_left_div"></div>
+                                    <p className={styles.prof}>Генеральный директор</p>
+                                </div>
+                            </Link>
                         </SwiperSlide>
-                        <SwiperSlide>
-                            <Image
-                                src="/remove_from_server/image-4.jpg"
-                                alt="specialist_1"
-                                width={466}
-                                height={342}
-                                className={`dsv-image`}
-                            />
+                        <SwiperSlide className={styles.item}>
+                            <Link href="/">
+                                <div className={styles.item_image}>
+                                    <Image
+                                        src="/remove_from_server/image-4.jpg"
+                                        alt="specialist_1"
+                                        width={466}
+                                        height={342}
+                                        className={`dsv-image`}
+                                    />
+                                </div>
+                                <div className={styles.item_content}>
+                                    <div className={styles.item_inner}>
+                                        <h3 className={styles.item_title}>науменко мария
+                                            андреевна</h3>
+                                        <svg width="36" height="35" viewBox="0 0 36 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M8.9007 26.289L26.5784 8.61133" stroke="#27272A" strokeWidth="2" />
+                                            <path d="M10.6826 8.55835H27.7992" stroke="#27272A" strokeWidth="2" />
+                                            <path d="M26.7861 24.6619V7.54529" stroke="#27272A" strokeWidth="2" />
+                                        </svg>
+                                    </div>
+                                    <div className="border_left_div"></div>
+                                    <p className={styles.prof}>Врач стоматолог терапевт, эндодонтист,
+                                        реставратор</p>
+                                </div>
+                            </Link>
                         </SwiperSlide>
-                        <SwiperSlide>
-                            <Image
-                                src="/remove_from_server/image-5.jpg"
-                                alt="specialist_1"
-                                width={466}
-                                height={342}
-                                className={`dsv-image`}
-                            />
+                        <SwiperSlide className={styles.item}>
+                            <Link href="/">
+                                <div className={styles.item_image}>
+                                    <Image
+                                        src="/remove_from_server/image-5.jpg"
+                                        alt="specialist_1"
+                                        width={466}
+                                        height={342}
+                                        className={`dsv-image`}
+                                    />
+                                </div>
+                                <div className={styles.item_content}>
+                                    <div className={styles.item_inner}>
+                                        <h3 className={styles.item_title}>капустина юлия
+                                            сергеевна</h3>
+                                        <svg width="36" height="35" viewBox="0 0 36 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M8.9007 26.289L26.5784 8.61133" stroke="#27272A" strokeWidth="2" />
+                                            <path d="M10.6826 8.55835H27.7992" stroke="#27272A" strokeWidth="2" />
+                                            <path d="M26.7861 24.6619V7.54529" stroke="#27272A" strokeWidth="2" />
+                                        </svg>
+                                    </div>
+                                    <div className="border_left_div"></div>
+                                    <p className={styles.prof}>Врач реставратор, пародонтолог</p>
+                                </div>
+                            </Link>
                         </SwiperSlide>
-                        <SwiperSlide>
-                            <Image
-                                src="/remove_from_server/image-3.jpg"
-                                alt="specialist_1"
-                                width={466}
-                                height={342}
-                                className={`dsv-image`}
-                            />
+                        <SwiperSlide className={styles.item}>
+                            <Link href="/">
+                                <div className={styles.item_image}>
+                                    <Image
+                                        src="/remove_from_server/image-3.jpg"
+                                        alt="specialist_1"
+                                        width={466}
+                                        height={342}
+                                        className={`dsv-image`}
+                                    />
+                                </div>
+                                <div className={styles.item_content}>
+                                    <div className={styles.item_inner}>
+                                        <h3 className={styles.item_title}>дубровина Анастасия александровна</h3>
+                                        <svg width="36" height="35" viewBox="0 0 36 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M8.9007 26.289L26.5784 8.61133" stroke="#27272A" strokeWidth="2" />
+                                            <path d="M10.6826 8.55835H27.7992" stroke="#27272A" strokeWidth="2" />
+                                            <path d="M26.7861 24.6619V7.54529" stroke="#27272A" strokeWidth="2" />
+                                        </svg>
+                                    </div>
+                                    <div className="border_left_div"></div>
+                                    <p className={styles.prof}>Генеральный директор</p>
+                                </div>
+                            </Link>
                         </SwiperSlide>
 
                         <SwiperNavButtons
