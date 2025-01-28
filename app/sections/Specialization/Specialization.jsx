6af4a3c027ad12776a7defa53f767ecdd78213e0
kkirrin <khlebnikov-2016@mail.ru>
2025-01-28 14:37:50 +1000

    feat: добавляет секцию Специальности

---
+++ app/sections/Specialization/Specialization.jsx
@@ -0,0 +1,16 @@
+import styles from './style.module.css';
+
+export default function Specialization() {
+    return (
+        <section className='section'>
+            <div className='container'>
+                <h2 className='title'>наша специализация</h2>
+                <p className={styles.subtitle}>Лечим зубы, как лечили бы себе</p>
+
+                <div className={styles.specialization_wrapper}>
+                    
+                </div>
+            </div>
+        </section>
+    )
+}
