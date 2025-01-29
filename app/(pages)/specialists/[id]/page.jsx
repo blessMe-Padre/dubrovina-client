3715ef69410ba54383ca7a25a200b7629c5350a6
Сергей Субботин <zarodiny@yandex.ru>
2025-01-29 12:06:26 +1000

    feat: добавляет sass

--- app/(pages)/specialists/[id]/page.jsx
+++ app/(pages)/specialists/[id]/page.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+'use client';
+import { useEffect, useState } from 'react';
+
+import styles from './style.module.scss';
+import Image from 'next/image';
+import { Button, Popup } from '@/app/components';
 
 export default function page({ params }) {
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [popupActive, setPopupActive] = useState(false);
     const { id } = params;
+
+    const handleClick = () => {
+        setPopupActive(true);
+    }
 
 
     if (!page) {
@@ -9,6 +21,34 @@ export default function page({ params }) {
     }
 
     return (
-        <div>Специалист с id = {id}</div>
+        <div className="container">
+            Специалист с id = {id}
+            <div className='section'>
+                <div className={styles.image_wrapper}>
+                    <Image
+                        src='/placeholders/specialist_id.jpg'
+                        alt='Dubrovina logo'
+                        width={1439}
+                        height={850}
+                        className="dsv-image"
+                    />
+                </div>
+
+                <header className={styles.header}>
+                    <div>
+                        <h1 className={styles.title}>дубровина екатерина сергеевна</h1>
+                        <p className={styles.subtitle}>Ведущий ортопед, хирург имплантолог клиники</p>
+                    </div>
+                    <Button
+                        color='black'
+                        handleClick={handleClick}
+                        href="#popup"
+                        size='medium'
+                        text='Записаться'></Button>
+                </header>
+            </div>
+
+            <Popup active={popupActive} setActive={setPopupActive} />
+        </div>
     )
 }
