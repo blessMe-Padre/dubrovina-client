5572e7b1cdcf113582476721a64b1f955dc50024
kkirrin <khlebnikov-2016@mail.ru>
2025-02-05 16:31:56 +1000

    feat: нужно закончить страницу О клинике

--- app/(pages)/about_clinic/PageContent.jsx
+++ app/(pages)/about_clinic/PageContent.jsx
@@ -1,14 +1,25 @@
 'use client';
 
+import { Breadcrumbs } from '@/app/components';
 import styles from './style.module.scss';
 
-import { VideoSection } from '@/app/sections';
+import { VideoSection, Mission, Statistic, Advantages, Consultation, Map, Trust } from '@/app/sections';
 export default function ContentPage({ data }) {
 
 
     return (
-        <>
+        <>  
+            <Breadcrumbs
+                secondLink="/about_clinic"
+                secondLabel="О клинике"
+            />
             <VideoSection all_width={true} />
+            <Mission />
+            <Statistic />
+            <Advantages />
+            <Trust />
+            <Consultation />
+            <Map />
         </>
     );
 }
