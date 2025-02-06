327a4541d5497998bdec3ef993d0e66b1300de3f
Сергей Субботин <zarodiny@yandex.ru>
2025-02-07 09:24:06 +1000

    feat: добавляет адаптив

--- app/(pages)/for-patients/page.jsx
+++ app/(pages)/for-patients/page.jsx
@@ -1,6 +1,6 @@
 import styles from './style.module.scss';
 import Image from 'next/image';
-import { Breadcrumbs } from "@/app/components";
+import { Breadcrumbs, LinkButton } from "@/app/components";
 
 export default function page() {
     return (
@@ -25,6 +25,34 @@ export default function page() {
                     <h1 className='title'>лечение без страха</h1>
                     <p className={styles.section_text}>Если вы испытываете страх перед визитом
                         к стоматологу. Мы сделаем всё возможное, чтобы помочь вам справиться с дискомфортными ощущениями</p>
+                </div>
+            </section>
+
+            <section className={styles.price_list}>
+                <div className="container">
+                    <div className={styles.price_wrapper}>
+                        <div className={styles.price_list_block}>
+                            <h2 className={styles.price_title}>Прейскурант</h2>
+                            <LinkButton
+                                link={'/price'}
+                                text='Посмотреть прайс'
+                                color='black'
+                            />
+                        </div>
+
+                        <div className={styles.price_wrapper_img}>
+                            <Image
+                                src='/images/image-6.jpg'
+                                alt='Dubrovina logo'
+                                width={1000}
+                                height={507}
+                                className="dsv-image"
+                                placeholder="blur"
+                                blurDataURL="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTQ0MiIgaGVpZ2h0PSIxMTg5IiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxyZWN0IHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIGZpbGw9IiNjY2MiIC8+PC9zdmc+" priority
+                            />
+                        </div>
+
+                    </div>
                 </div>
             </section>
         </>
