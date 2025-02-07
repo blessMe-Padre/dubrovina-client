c9d03a397fc05beba9c08d49392c8a3f45bc00a3
Сергей Субботин <zarodiny@yandex.ru>
2025-02-07 11:24:44 +1000

    feat: добавляет api

--- app/sections/DocumentSection/DocumentSection.jsx
+++ app/sections/DocumentSection/DocumentSection.jsx
@@ -1,34 +1,39 @@
 
 import { Document } from '@/app/components';
 import styles from './style.module.scss';
-import Image from 'next/image';
+import getData from '../../utils/getData';
 
+export default async function DocumentSection() {
 
-export default function DocumentSection() {
+    let data = null;
+    try {
+        const response = await getData(`${process.env.NEXT_PUBLIC_DOMAIN}/api/my-documents?populate=*`);
+        data = response || null;
+
+    } catch (error) {
+        console.error('Ошибка при загрузке данных:', error);
+    }
+
     return (
         <section className={styles.section}>
             <div className="container">
                 <h2 className='title title--black'>документы</h2>
 
                 <ul className={styles.documents_list}>
-                    <li>
-                        <Document
-                            text="Лицензия № Л041-01137-77/00322452 от 28 июня 2018г"
-                            link="/"
-                        />
-                    </li>
-                    <li>
-                        <Document
-                            text="Лицензия № Л041-01137-77/00322452 от 28 июня 2018г"
-                            link="/"
-                        />
-                    </li>
-                    <li>
-                        <Document
-                            text="Лицензия № Л041-01137-77/00322452 от 28 июня 2018г"
-                            link="/"
-                        />
-                    </li>
+
+                    {data?.data?.length > 0 ? (
+                        data.data.map((item, index) => (
+                            <li key={index}>
+                                <Document
+                                    text={item.name}
+                                    link={item?.my_file?.url}
+                                />
+                            </li>
+                        ))
+                    ) : (
+                        <p>Нет загруженных документов</p>
+                    )}
+
                 </ul>
             </div>
         </section>
