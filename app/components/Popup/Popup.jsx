340c0cdb06259419cc5b230249d5be22ce27106e
kkirrin <khlebnikov-2016@mail.ru>
2025-02-25 11:46:28 +1000

    fix: основные правки внесены

--- app/components/Popup/Popup.jsx
+++ app/components/Popup/Popup.jsx
@@ -34,8 +34,8 @@ export default function Popup({ active, setActive }) {
                         onClick={() => { setActive(false) }}
                     >
                         <svg width="23" height="22" viewBox="0 0 23 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <rect x="3" y="18.4463" width="21.8033" height="0.726776" rx="0.363388" transform="rotate(-45 3 18.4463)" fill="#2a3a57" />
-                            <rect x="4.21094" y="3" width="21.8033" height="0.726776" rx="0.363388" transform="rotate(45 4.21094 3)" fill="#2a3a57" />
+                            <rect x="3" y="18.4463" width="21.8033" height="0.726776" rx="0.363388" transform="rotate(-45 3 18.4463)" fill="#fff" />
+                            <rect x="4.21094" y="3" width="21.8033" height="0.726776" rx="0.363388" transform="rotate(45 4.21094 3)" fill="#fff" />
                         </svg>
                     </button>
 
