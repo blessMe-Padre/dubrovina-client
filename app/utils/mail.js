d538861aa375e27143eda50f6b3603d90b447776
Сергей Субботин <zarodiny@yandex.ru>
2025-08-04 10:41:24 +1000

    feat: добавляет новый сервер

--- app/utils/mail.js
+++ app/utils/mail.js
@@ -14,7 +14,8 @@ async function sendEmail(body) {
     try {
         const info = await transporter.sendMail({
             from: process.env.SMTP_FROM,
-            to: process.env.SMTP_FROM,
+            // from: 'no-replay@dubrovinastom.ru',
+            to: `${process.env.SMTP_FROM}, Dubrovinaaa@yandex.ru`,
             subject: "Форма с сайта dubrovinastom.ru",
             text: `Имя: ${body.name}\nТелефон: ${body.phone}`,
             html: `<b>Имя:</b> ${body.name}<br><b>Телефон:</b> ${body.phone}`,
